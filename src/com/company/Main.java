0ed45a6e86ed3ef93cfb86e454fdfc72f34ade99
Dream-By <dzmitry.dzianisau@gmail.com>
2020-05-11 15:38:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex{
+    //first match 25_ series then 2__ series and then remainings.
+    String pattern="^((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])$";
+}
